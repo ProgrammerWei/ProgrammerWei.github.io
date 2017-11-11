8c362eb8caae929111d24d56ee37558845a2332c
ProgrammerWei <2217148398@qq.com>
2017-11-11 20:15:05 +0800

    Site updated: 2017-11-11 20:15:04

--- js/photo.js
+++ js/photo.js
@@ -19,7 +19,7 @@
                 for (var i = begin; i < end && i < data.length; i++) {
                     li += '<li><div class="img-box">' +
                         '<a class="img-bg" rel="example_group" href="https://github.com/ProgrammerWei/img/tree/master/blog/' + data[i] + '?raw=true"></a>' +
-                        '<img lazy-src="https://github.com/lwzhang/blog/blob/master/photos/' + data[i] + '?raw=true" />' +
+                        '<img lazy-src="https://github.com/ProgrammerWei/img/tree/master/blog/' + data[i] + '?raw=true" />' +
                         '</li>';
                 }
 
